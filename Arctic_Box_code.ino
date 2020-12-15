fa0f8d57063877675e99f170b129442a5e574d28
Sean Jeong <sean_jeong@sfu.ca>
2020-12-14 18:12:11 -0800

    Updated Arctic Box code
    Updated for capacitive touch sensor

--- Arctic_Box_code.ino
+++ Arctic_Box_code.ino
@@ -1,8 +1,8 @@
-int sensorpin = 0;
-int val = 0;
+#include <Adafruit_CircuitPlayground.h>
 
+int vals[8] = {3,2,0,1,10,6,9,12};
 
-int ledpin = 13;
+int ledPin = 13;
 int ledState = LOW;    
 
 void setup() {
@@ -12,15 +12,14 @@ void setup() {
 }
 
 void loop() {
- val = analogRead(sensorpin);
- 
- if(val >300) {
-  ledState = HIGH;
-  
- } else {
+  for (int i=0;i<8;i++){
+   int x = CircuitPlayground.readCap(vals[i]);
+   if (x > 600) {
+         ledState = HIGH;
+     } else {
   ledState = LOW;
  }
-
+  }
 
   digitalWrite(ledPin, ledState);
 }
